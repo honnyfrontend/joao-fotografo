1ddea5b3fa74a6993f5be0ffcf792eaa7023cbfd
honny batista de oliveira <93882631+honnyfrontend@users.noreply.github.com>
2025-07-30 21:24:54 -0300

    erro do PATH nao autorizado no cors

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,7 @@ const allowedOrigins = [
 
 app.use(cors({
   origin: allowedOrigins,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], // <-- Adicione 'PATCH' aqui!
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 }));
